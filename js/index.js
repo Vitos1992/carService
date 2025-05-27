e775361bd6493d30f30aa56e9c9d4cf1afd8e2be
Vitos1992 <125671703+Vitos1992@users.noreply.github.com>
2025-05-27 23:14:21 +0300

    newqwe

--- js/index.js
+++ js/index.js
@@ -1,50 +1,67 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    loop: true,
-    slidesPerView: 3,
-    spaseBetween: 60,
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     loop: true,
+//     slidesPerView: 3,
+//     spaseBetween: 60,
 
     
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true,
-        // type: 'fraction',
-    },
+//     // If we need pagination
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//         dynamicBullets: true,
+//         // type: 'fraction',
+//     },
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
 
 
 
-    // touchRation: 1,
+//     // touchRation: 1,
 
-    // grabCursor: true,
+//     // grabCursor: true,
 
-    // keyboard: {
-    //     enable: true,
-    //     onlyInViewport: true,
-    //     pageUpDown: true,
-    // },
+//     // keyboard: {
+//     //     enable: true,
+//     //     onlyInViewport: true,
+//     //     pageUpDown: true,
+//     // },
 
-    mousewheel: {
-        sensitivity: 1,
-    },
+//     mousewheel: {
+//         sensitivity: 1,
+//     },
 
-    avtoHeight: true,
+//     avtoHeight: true,
 
-    effect: 'coverflow',
+//     effect: 'coverflow',
 
-    coverflowEffect: {
-        rotate: 20,
-        stretch: 50,
-        sliderShadows: true,
-    }
+//     coverflowEffect: {
+//         rotate: 20,
+//         stretch: 50,
+//         sliderShadows: true,
+//     }
 
-});
+// });
+
+const swiper = new Swiper(".swiper", {
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+    });
+    var swiper2 = new Swiper(".swiper", {
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      thumbs: {
+        swiper: swiper,
+      },
+    });
 
