2fdd79a14847b93b7b986e421b9b10e607e73d34
Vitos1992 <125671703+Vitos1992@users.noreply.github.com>
2025-05-28 14:23:13 +0300

    ewq

--- js/index.js
+++ js/index.js
@@ -1,67 +1,19 @@
-// const swiper = new Swiper('.swiper', {
-//     // Optional parameters
-//     loop: true,
-//     slidesPerView: 3,
-//     spaseBetween: 60,
-
-    
-
-//     // If we need pagination
-//     pagination: {
-//         el: '.swiper-pagination',
-//         clickable: true,
-//         dynamicBullets: true,
-//         // type: 'fraction',
-//     },
-
-//     // Navigation arrows
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
-
-
-
-//     // touchRation: 1,
-
-//     // grabCursor: true,
-
-//     // keyboard: {
-//     //     enable: true,
-//     //     onlyInViewport: true,
-//     //     pageUpDown: true,
-//     // },
-
-//     mousewheel: {
-//         sensitivity: 1,
-//     },
-
-//     avtoHeight: true,
-
-//     effect: 'coverflow',
-
-//     coverflowEffect: {
-//         rotate: 20,
-//         stretch: 50,
-//         sliderShadows: true,
-//     }
-
-// });
-
-const swiper = new Swiper(".swiper", {
-      spaceBetween: 10,
-      slidesPerView: 4,
-      freeMode: true,
-      watchSlidesProgress: true,
-    });
-    var swiper2 = new Swiper(".swiper", {
-      spaceBetween: 10,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      thumbs: {
-        swiper: swiper,
-      },
-    });
-
+$(document).ready(function(){
+  $('.slider').slick({
+    dots: true,
+    adaptiveHeight: true,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    speed: 1000,
+    easing:'_lianer',
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    pauseOnFocus: true,
+    pauseOnFocusHover: true,
+    pauseOnFocusHoverDots: true,
+    draggable: false,
+    waitForAnimate: false,
+  });
+  
+});
