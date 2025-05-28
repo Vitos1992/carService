e7927bd4b15f1a4f1656fb23a7c8b269e455398f
Vitos1992 <125671703+Vitos1992@users.noreply.github.com>
2025-05-28 17:23:31 +0300

    newwq

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('.slider').slick({
     dots: true,
     adaptiveHeight: true,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 2,
     speed: 1000,
     easing:'_lianer',
@@ -14,6 +14,14 @@ $(document).ready(function(){
     pauseOnFocusHoverDots: true,
     draggable: false,
     waitForAnimate: false,
+    asNavFor: ".sliderbig",
   });
-  
+  $('.sliderbig').slick({
+    arrows: false,
+    fade: true,
+    asNavFor: ".slider",
+
+
+  });
 });
+  
