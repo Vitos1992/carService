88848b4013be4378a74b13dca90c25247f2db99f
Vitos1992 <125671703+Vitos1992@users.noreply.github.com>
2025-06-01 12:23:14 +0300

    new

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('.slider').slick({
     dots: true,
     adaptiveHeight: true,
-    slidesToShow: 4,
+    slidesToShow: 5,
     slidesToScroll: 2,
     speed: 1000,
     easing:'_lianer',
@@ -14,14 +14,6 @@ $(document).ready(function(){
     pauseOnFocusHoverDots: true,
     draggable: false,
     waitForAnimate: false,
-    asNavFor: ".sliderbig",
-  });
-  $('.sliderbig').slick({
-    arrows: false,
-    fade: true,
-    asNavFor: ".slider",
-
-
   });
 });
   
