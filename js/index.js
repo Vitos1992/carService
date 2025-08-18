091bd5b8bd120ef6c0a19c3e7ba0db79559260a1
Vitos1992 <125671703+Vitos1992@users.noreply.github.com>
2025-08-18 23:29:02 +0300

    newrr

--- js/index.js
+++ js/index.js
@@ -36,20 +36,28 @@ $(document).ready(function(){
     ]
   });
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("burger").addEventListener("click", function() {
+    document.querySelector("header_mob-soc_menu").classList.toggle("is-hidden")
+  })
+})
+                                                                         
   
 
 
-(() => {
-  const refs = {
-    openMenuBtn: document.querySelector("[data-menu-open]"),
-    closeMenuBtn: document.querySelector("[data-menu-close]"),
-    menu: document.querySelector("[data-menu]"),
-  };
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector("[data-menu-open]"),
+//     closeMenuBtn: document.querySelector("[data-menu-close]"),
+//     menu: document.querySelector("[data-menu]"),
+//   };
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+//   refs.openMenuBtn.addEventListener("click", toggleMenu);
+//   refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
-  function toggleMenu() {
-      refs.menu.classList.toggle("is-hidden");
-  }
-})();
+//   function toggleMenu() {
+//       refs.menu.classList.toggle("is-hidden");
+//   }
+// })();
